e5c6953d438718cbd9a9dd66b3e36eac5bde64e0
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2025-03-24 10:19:47 +0530

    ,

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ import morgan from "morgan";
 const app = express();
 
 
+
 app.use(
   cors({
-    origin:  "https://sakhiappbackend.onrender.com",
+    origin: "https://sakhiappbackend.onrender.com",
+    methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
 );
+app.options("*", cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
