b75afd75928f4d900dc4cb6a886f054998c7a8c7
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2025-03-24 10:14:00 +0530

    ,

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173" , "https://sakhiappbackend.onrender.com"],
+    origin:  "https://sakhiappbackend.onrender.com",
     credentials: true,
   })
 );
