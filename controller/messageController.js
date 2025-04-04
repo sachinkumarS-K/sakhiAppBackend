a46824c2de21b29b36f17e6656805e5191d356a7
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2025-04-04 15:23:27 +0530

    .

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import twilio from "twilio";
+import { accountSid, authToken, twilioNumber } from "../utils/constant.js";
 
-const accountSid = "AC3a1bb9140977e5536864783373bf7aea";
-const authToken = "fbfb3d6dee00173b512b2439fe03265f";
-const twilioNumber = "+12513124829";
+// const accountSid = "AC3a1bb9140977e5536864783373bf7aea";
+// const authToken = "fbfb3d6dee00173b512b2439fe03265f";
+// const twilioNumber = "+12513124829";
 
-console.log(accountSid , authToken,twilioNumber)
+// console.log(accountSid , authToken,twilioNumber)
 if (!accountSid || !authToken) {
   console.error(" Twilio credentials are missing!");
   // process.exit(1);
