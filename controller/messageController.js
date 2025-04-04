c1b7d9042c78ae75c14b40a699a505ee0375df80
sachinkumarS-K <sachinraj.s.r.123456@gmail.com>
2025-04-04 08:11:40 +0530

    a

--- controller/messageController.js
+++ controller/messageController.js
@@ -5,9 +5,10 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioNumber = process.env.TWILIO_PHONE_NUMBER;
 
+console.log(accountSid , authToken,twilioNumber)
 if (!accountSid || !authToken) {
-  console.error("❌ Twilio credentials are missing!");
-  process.exit(1);
+  console.error(" Twilio credentials are missing!");
+  // process.exit(1);
 }
 
 
